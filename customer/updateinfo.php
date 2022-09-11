046b4fc6aa3103554308761eb3e0a2830deb1994
Cristian Vargas <cristiandavidvargasmesa@gmail.com>
2022-09-10 21:35:36 -0500

    Actualizar informacion

--- customer/updateinfo.php
+++ customer/updateinfo.php
@@ -18,6 +18,13 @@ if(!isset($_SESSION['userinfo'])){
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@300&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="../profile_styles.css">
+    <style>
+        .main_form{font-size:0.9em;background-color:#ccc;display:flex;justify-content:space-around;flex-direction:column;align-items:center;min-height:530px;height:auto;font-weight:bold;box-shadow:5px 5px 20px 5px #333;padding:10px;}
+        .main_form input[type="text"], input[type="email"]{width:70%;text-align:center;height:25px;border-radius:15px;}
+        .submit_button{background-color:#a1ca4f;width:45%;height:35px;font-weight:bold;border-radius:15px;font-size:1.2em;}
+        .submit_button:hover{background-color:#85b427;}
+        .submit_button:active{background-color:black;color:white;}
+    </style>
     <script>
 
     function logout(){
@@ -51,18 +58,22 @@ if(!isset($_SESSION['userinfo'])){
                 <h3>Contactos:</h3><p>301xxx xx xx<br>3022459827</p>
             </div>
             <div>
-            <form action="<?php echo $_SERVER['PHP_SELF'];?>" method="POST">
-            <h3>Ingresa sólo los datos a actualizar</h3>
+            <form action="<?php echo $_SERVER['PHP_SELF'];?>" method="POST" class="main_form">
                 <label>Nombre</label>
                 <input type="text" name="name" placeholder="Ingresa un nuevo nombre">
                 <label>Cédula</label>
                 <input type="text" name="id" placeholder="Ingresa un nuevo N° de identidad">
                 <label>Email</label>
-                <input type="text" name="email" placeholder="Ingresa un nuevo correo electrónico">
+                <input type="email" name="email" placeholder="Ingresa un nuevo correo electrónico">
                 <label>Celular</label>
                 <input type="text" name="cell" placeholder="Ingresa un nuevo N° de celular">
                 <label>Dirección de residencia</label>
                 <input type="text" name="direction" placeholder="Ingresa una nueva dirección">
+                <label>Ciudad</label>
+                <input type="text" name="city" placeholder="Ingrese una nueva ciudad">
+                <label>Departamento</label>
+                <input type="text" name="departament" placeholder="Ingrese un nuevo departamento">
+                <input class="submit_button" type="submit" name="send" value="Actualizar">
             </form>
             </div>
             <div class="function">
--- main/login.php
+++ main/login.php
@@ -112,6 +112,7 @@ if(isset($_POST['login'])){
             } 
         }
     $connection->close();
+    $adminconnection->close();
 }
 
 ?>
